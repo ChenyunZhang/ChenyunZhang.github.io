6a8896f58851116674732b68db6e40f39fc3098d
Chenyun Zhang <morningchenyun@gmail.com>
2021-03-28 20:51:20 -0400

    ts

---
+++ 6_Type_script/fundamentals.ts
@@ -0,0 +1,36 @@
+// strong typing
+// Object-oriented features
+// compile-time errors
+// great tooling
+
+// TypeScript ===> JS
+
+// let log = (message) =>{
+//     console.log(message);
+// }
+// log("message");
+
+// let doSomething = ()=>{
+//     let i = 1
+//     while(i<=5){
+//         console.log(i)
+//         i++
+//     }
+// }
+// doSomething()
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] 
+let f: any[] = [1,true,"a",false]
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color {Red=0,Green=1,Blue=2};
+let backgroundColoe = Color.Green
+
+
